f24b4447cd480fdaa74196e8b4f6deee186ad60a
Nicholas <npere017@ucr.edu>
2019-03-21 14:29:20 +0000

    Set up plan of action for program.

--- time_calc.cpp
+++ time_calc.cpp
@@ -8,6 +8,44 @@ using namespace std;
 
 int main()
 {
+    string name;
+    
+    // Get name for now, for a more personal experience throughout the program.
+    cout << "Please enter your name: ";
+    getline(cin, name);
+    
+    // Testing for name. Delete later.
+    cout << "Your name is: " << name << "!" << endl << endl;
+    
+    // Ask for start hour, 24 hr format.
+    
+    // Ask for start minute.
+    
+    // Ask for ending hour, 24 hr format.
+    
+    // Ask for ending minute.
+    
+    // Begin loop for breaks. Ask for number of breaks during work day, not to exceed 5. Place in 2 arrays (hr and min).
+        // Ask for lunch hour start, 24 hr format.
+        
+        // Ask for lunch hour end minute.
+        
+        // Ask for lunch hour end, 24 hr format.
+        
+        // Ask for lunch minute end, 24 hr format.
+    // End loop for breaks.
+    
+    // Convert all times over to minutes.
+    
+    // Find range time in minutes of overall work day
+    
+    // Convert breaks into minutes. Add hr[i] with time[i] together. Place result in mins. in new array.
+    
+    // Subtract all values of new array (breaks) from total time.
+    
+    // Convert back to hours and minutes of total working time.
+    
+    // Output data. Give range (full working day) and then each break (above 0). Below that, give total time worked in hours and minutes. 
     
     
     system("pause");
