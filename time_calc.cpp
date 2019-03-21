1a95e62882c84cd27813345ac091da77deba3dc1
Nicholas <npere017@ucr.edu>
2019-03-21 09:42:58 +0000

    Start of main cpp file and test push

---
+++ time_calc.cpp
@@ -0,0 +1,15 @@
+// Nicholas Perez-Aguilar
+// Start Date: 21 March 2019
+// Description: This will be a program that will compute times to display how many hours/minutes you worked, with time on/off.
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    
+    
+    system("pause");
+    return 0;
+}
